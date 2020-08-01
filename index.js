b568a9e529136c2fce9edd7937fda8c1a4beb4b6
mdragic04 <53371014+mdragic04@users.noreply.github.com>
2020-08-01 18:08:50 +0200

    GET track by id

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-import express from 'express';
-import fs from 'fs';
+const express = require('express');
+const data = require('./zadatak.json')
 
 
 const app = express();
 const port = 3000;
 
-//Loading JSON file for later use
-const data = JSON.parse(fs.readFileSync('zadatak.json'));
 
 //GET request to /tracks returs all tracks
 app.get('/tracks', (req, res) => {
-    res.contentType('aplication/json');
-    res.send(data["tracks"]);
+    res.json(data["tracks"]);
 });
 
+app.get('/tracks/:id', (req, res) => {
+    const trackById = data["tracks"].data.find((t) => t.id === parseInt(req.params.id));
+    res.json(trackById);
 
+});
 
 app.listen(port, () => console.log(`App is listening at http://localhost:${port}`));
